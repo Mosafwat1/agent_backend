10fcca07393ceef08b987d925d26905f98cafb55
Mohamed Safwat <Mohamed.Safwat@UTP.local>
2025-01-19 14:55:24 +0200

    localhost hello_world

--- src/loaders/homeLoader.ts
+++ src/loaders/homeLoader.ts
@@ -1,21 +1,31 @@
 import * as express from 'express';
 import { MicroframeworkLoader, MicroframeworkSettings } from 'microframework-w3tec';
-
 import { env } from '../env';
+
+export const expressLoader: MicroframeworkLoader = (settings: MicroframeworkSettings | undefined) => {
+    if (settings) {
+        const app = settings.getData('express_app'); // Retrieve the express instance
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
+    }
+};
 
 export const homeLoader: MicroframeworkLoader = (settings: MicroframeworkSettings | undefined) => {
     if (settings) {
-        const expressApp = settings.getData('express_app');
-        expressApp.get(
-            env.app.routes.prefix,
-            (req: express.Request, res: express.Response) => {
-                return res.json({
-                    name: env.app.name,
-                    version: env.app.version,
-                    description: env.app.description,
-                });
-            }
-        );
+        const app = settings.getData('express_app'); // Retrieve the express instance
+        
+        // Existing route
+        app.get(env.app.routes.prefix, (req: express.Request, res: express.Response) => {
+            return res.json({
+                name: env.app.name,
+                version: env.app.version,
+                description: env.app.description,
+            });
+        });
 
+        // New "Hello, Data!" route
+        app.get('/', (req: express.Request, res: express.Response) => {
+            res.send('Hello, Data!');
+        });
     }
 };
