1c18c023d9bf4fba0c58d4c55458ffa1e57f0af7
ashraf.amer@ultratech-plus.com <ashraf.amer@ultratech-plus.com>
2025-01-20 14:58:08 +0200

    encrypt requests

---
+++ src/api/middlewares/SignatureMiddleware.ts
@@ -0,0 +1,30 @@
+import * as express from 'express';
+import * as crypto from 'crypto';
+import { ExpressMiddlewareInterface, Middleware } from 'routing-controllers';
+
+@Middleware({ type: 'before' })
+export class SignatureMiddleware implements ExpressMiddlewareInterface {
+
+    public use(req: express.Request, res: express.Response, next: express.NextFunction): any {
+
+        try {
+            const { secret } = req.headers;
+            const { data } = req.body || process.env.SIGNATURE_KEY;
+            const signature = crypto.createHmac('sha256', process.env.SIGNATURE_SECRET).update(data).digest('hex');
+
+            // key unused right now
+            if (!data || !secret || signature !== secret) {
+                throw new Error('Bad Request');
+            }
+
+            return next();
+        } catch (err) {
+            console.log(err);
+            return res.status(400).json({
+                isSuccess: false,
+                message: 'Bad Request',
+            });
+        }
+    }
+
+}
